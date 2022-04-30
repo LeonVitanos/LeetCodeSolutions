616caf42eb2fd8e3452e0be1f492daf39f878c2a
Leon Vitanos <leon.vitanos@gmail.com>
2022-04-30 18:15:03 +0200

    solved recursively

--- 5.LongestPalindromicSubstring.cpp
+++ 5.LongestPalindromicSubstring.cpp
@@ -8,56 +8,38 @@ using namespace std;
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        string longest_palindrome;
-        auto size = s.size();     
-
-        string substring = s;
-        for (int i=0; i<size; i++){
-            for(int k=0; k<substring.size(); k++){
-                
-                // We remove from right
-                string temp;
-                for(int l=0; l<substring.size()-k; l++){
-                    temp.push_back(substring[l]);
-                }
-                auto temp_size = temp.size();
-                
-                // Check if substring is palindromic
-                bool flag=true;
-                for(int j=0; j<temp_size/2; j++){                    
-                    if(temp[j]!=temp[temp_size-j-1]){ // Not palindromic
-                        flag=false;
-                        break;
-                    }
-                }
-
-                // If palindromic and bigger than already found, update
-                if(flag && substring.length()-k>longest_palindrome.length()){
-                    longest_palindrome.clear();
-                    for (char c: temp){
-                        longest_palindrome.push_back(c);
-                    }
-                }
-            }
-
-            // Terminate if only one char
-            if(substring.size()==1){
-                if(longest_palindrome.empty())
-                    longest_palindrome=substring;
-                break;
-            }
-
-            // Remove most left character
-            string temp;
-            for (int i=1; i<substring.size(); i++){
-                temp.push_back(substring[i]);
-            }
-            substring.clear();
-            substring=temp;
+    string findPalindrome(string s, int left, int right)
+    {
+        int n = s.size();
+        int l = left;
+        int r = right;
+        while (left>=0 && right<=n-1 && s[left] == s[right]) {
+            left--;
+            right++;
         }
-    return longest_palindrome;
-    } 
+        return s.substr(left+1, right-left-1);
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n<=1) return s;
+
+        string longest;
+
+        string str;
+        for (int i=0; i<n-1; i++) {
+            str = findPalindrome(s, i, i);
+            if (str.size() > longest.size()){
+                longest = str;
+            } 
+            str = findPalindrome(s, i, i+1);
+            if (str.size() > longest.size()){
+                longest = str;
+            } 
+        }
+
+        return longest;
+    }
 };
 
 int main() {
