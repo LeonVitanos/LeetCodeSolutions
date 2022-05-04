3e0307c7b00870e4e2daf8c9e65646a815f4f0f8
Leon Vitanos <leon.vitanos@gmail.com>
2022-05-04 13:37:15 +0200

    Update 234.PalindromeLinkedList.cpp

--- 234.PalindromeLinkedList.cpp
+++ 234.PalindromeLinkedList.cpp
@@ -12,22 +12,37 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
-public:
-    bool isPalindrome(ListNode* head)
+bool isPalindrome(ListNode* head)
     {
         //Find middle node
         ListNode *p1=head, *p2=head;
+        
         while (p2 && p2->next){
             p1 = p1->next;
             p2 = p2->next->next;
         }
-        
 
-        front = head;
-        return palindromeCheck(head);
-    }
-};
+        ListNode* pMid = p1;
+
+        //Reverse List starting from middle node
+        ListNode *prev = NULL;
+        
+        while(p1) {
+            ListNode* temp = p1->next;
+            p1->next = prev;
+            prev = p1;
+            p1 = temp;
+        }
+
+        //Check if two linked lists are the same, thus palindrome
+            //Infinite foor loop, similar to while
+        for(;head!=pMid; head=head->next, prev=prev->next) {
+            if (head->val != prev->val) {
+                return false;
+            }
+        }
+        return true;
+}
 
 int main() {
     ListNode* head = new ListNode(1);
